fef27cb9bf2bd5847c228f4815649a61ff7e0561
EunhaKyeong <eh0634a@gmail.com>
2020-10-25 23:27:58 +0900

    임신여부 입력값까지 받아오는 백 작업 완료

--- yakpago/model/model.py
+++ yakpago/model/model.py
@@ -1,3 +1,3 @@
-def printInputValue(age, weight, disease):
-    result = {'age':age, 'weight':weight, 'disease':disease}
+def printInputValue(age, weight, disease, pregnant):
+    result = {'age':age, 'weight':weight, 'disease':disease, 'pregnant':pregnant}
     return result
